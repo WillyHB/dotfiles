7c9829abf596fe9fdc0319d9d8972ba511b2b211
WillyHB <william.hansen.baird@icloud.com>
2024-07-09 00:25:04 +0200

    added two dotfiles

---
+++ nvim/lua/willy/plugins.lua
@@ -0,0 +1,27 @@
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup('willy/plugins/')
+require('lualine').setup()
+require('hardtime').setup()
+
+require('telescope').setup()
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files({hidden = true})<cr>")
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
---
+++ nvim/lua/willy/set.lua
@@ -0,0 +1,17 @@
+vim.opt.relativenumber = true
+vim.opt.number = true;
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.wrap = true;
+vim.g.showbreak = '↪'
+vim.opt.smartindent = true;
+
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.syntax = 'enable'
