d078ab9ec3230a4bf9cb8694783af11671ad2e08
William <william.hansen.baird@icloud.com>
2024-08-11 13:50:16 +0200

    cleaned up symlink file

--- makesymlink.sh
+++ makesymlink.sh
@@ -21,15 +21,15 @@ echo "done"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
-cd $dir
+cd $dir || exit
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv $configdir/$file ~/dotfiles_old/
+    mv $configdir/"$file" ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file/ $configdir/$file
+    ln -s $dir/"$file"/ $configdir/"$file"
 done
 
 tmux source $dir/tmux/.tmux.conf
